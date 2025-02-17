ddbd07829197876c992ffde930a42d2746200204
Ara Hovsepyan <areshovsepyan@yahoo.com>
2025-02-17 19:23:29 +0300

    AH-8 / feat / add technologies data and icons

---
+++ src/stores/technologies.js
@@ -0,0 +1,59 @@
+import { defineStore } from 'pinia'
+
+export const useTechnologiesStore = defineStore('technologies', {
+  state: () => ({
+    technologies: [
+      {
+        label: 'HTML',
+        img: {
+          alt: 'HTML icon',
+          icon: 'html',
+        },
+      },
+      {
+        label: 'CSS',
+        img: {
+          alt: 'CSS icon',
+          icon: 'css',
+        },
+      },
+      {
+        label: 'Javascript',
+        img: {
+          alt: 'Javascript icon',
+          icon: 'javascript',
+        },
+      },
+      {
+        label: 'Typescript',
+        img: {
+          alt: 'Typescript icon',
+          icon: 'typescript',
+        },
+      },
+      {
+        label: 'Vue',
+        img: {
+          alt: 'Vue icon',
+          icon: 'vue',
+        },
+      },
+      {
+        label: 'Nuxt',
+        img: {
+          alt: 'Nuxt icon',
+          icon: 'nuxt',
+        },
+      },
+      {
+        label: 'Vite',
+        img: {
+          alt: 'Vite icon',
+          icon: 'vite',
+        },
+      },
+    ],
+  }),
+  getters: {},
+  actions: {},
+})
