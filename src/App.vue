0876421bd3df353620307f64f6be12230f61cdab
Ara Hovsepyan <areshovsepyan@yahoo.com>
2025-02-21 01:29:52 +0300

    AH-22 / feat / add route-based dynamic transition

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-import TheHeader from '@/components/layout/TheHeader.vue'
-import TheFooter from '@/components/layout/TheFooter.vue'
+import TheHeader from '@/components/layout/TheHeader.vue';
+import TheFooter from '@/components/layout/TheFooter.vue';
+
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+router.afterEach((to, from) => {
+  const toId = to.meta.id;
+  const fromId = from.meta.id;
+
+  to.meta.transition = toId > fromId ? 'slide-left' : 'slide-right';
+});
 </script>
 
 <template>
   <TheHeader />
 
   <router-view v-slot="{ Component, route }">
-    <Transition name="fade" mode="out-in">
+    <Transition :name="route.meta.transition" mode="out-in">
       <main :key="route.fullPath">
         <component :is="Component" />
       </main>
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from '@/views/HomeView.vue'
-import EducationView from '@/views/EducationView.vue'
-import ExperienceView from '@/views/ExperienceView.vue'
-import ContactView from '@/views/ContactView.vue'
+import HomeView from '@/views/HomeView.vue';
+import EducationView from '@/views/EducationView.vue';
+import ExperienceView from '@/views/ExperienceView.vue';
+import ContactView from '@/views/ContactView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: HomeView },
-    { path: '/education', component: EducationView },
-    { path: '/experience', component: ExperienceView },
-    { path: '/contact', component: ContactView },
+    { path: '/', component: HomeView, meta: { title: 'Home', id: 0 } },
+    { path: '/education', component: EducationView, meta: { title: 'Education', id: 1 } },
+    { path: '/experience', component: ExperienceView, meta: { title: 'Experience', id: 2 } },
+    { path: '/contact', component: ContactView, meta: { title: 'Contact', id: 3 } },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -21,6 +21,6 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue'),
     // },
   ],
-})
+});
 
-export default router
+export default router;
