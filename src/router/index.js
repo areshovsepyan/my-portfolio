23a63961a974ea64017a8091cd34150340fc264a
Ara Hovsepyan <areshovsepyan@yahoo.com>
2025-03-04 00:31:06 +0400

    feat: add 404 not fount page with glitch annimation

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,13 @@ import EducationView from '@/views/EducationView.vue';
 import ExperienceView from '@/views/ExperienceView.vue';
 import ContactView from '@/views/ContactView.vue';
 
+import NotFound from '@/views/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', alias: '/home', component: HomeView, meta: { title: 'Home', id: 0 } },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     { path: '/education', component: EducationView, meta: { title: 'Education', id: 1 } },
     { path: '/experience', component: ExperienceView, meta: { title: 'Experience', id: 2 } },
     { path: '/contact', component: ContactView, meta: { title: 'Contact', id: 3 } },
---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useResize } from '@/composables/useResize';
+const { isOnMobile } = useResize();
+</script>
+
+<template>
+  <section class="not-found">
+    <h1 class="glitch">404</h1>
+    <p class="section-greeting">
+      &lt; Oops! This page doesn’t exist. <br v-if="isOnMobile" />
+      The link you <br v-if="!isOnMobile" />
+      followed may be broken, <br v-if="isOnMobile" />
+      or the page has been moved. /&gt;
+    </p>
+    <router-link to="/">Go Home</router-link>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+
+  @media (min-width: 1024px) {
+    padding: 3rem 2rem;
+  }
+}
+</style>
