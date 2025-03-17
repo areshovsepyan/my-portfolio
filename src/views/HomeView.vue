cf4132c2e7de83692ecde7ec3d6f1521b63c84c1
Ara Hovsepyan <areshovsepyan@yahoo.com>
2025-03-17 19:17:27 +0400

    feat: add dev warning on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { onMounted } from 'vue';
+import toast from '../../utils/toast';
 import IntroductionSection from '@/components/IntroductionSection.vue';
 import TechnologiesSection from '@/components/TechnologiesSection.vue';
 import ProjectsSection from '@/components/ProjectsSection.vue';
 import TestimonialsSection from '@/components/TestimonialsSection.vue';
+
+onMounted(() => {
+  toast.warning(
+    {
+      title: 'Website Under Development',
+      text: 'This website is currently in development. Most of the data shown is mock data and subject to change. Stay tuned for updates!',
+    },
+    {
+      timeout: 10000,
+    },
+  );
+});
 </script>
 
 <template>
--- utils/toast.js
+++ utils/toast.js
@@ -1,13 +1,21 @@
+import { h } from 'vue';
 import { useToast } from 'vue-toastification';
 
 const toast = useToast();
 
-export const error = (error) => {
-  toast.error(typeof error === 'object' ? error.message : error);
+export const error = (error, config) => {
+  toast.error(typeof error === 'object' ? error.message : error, config);
 };
 
-export const success = (message) => {
-  toast.success(message);
+export const success = (message, config) => {
+  toast.success(message, config);
 };
 
-export default { error, success };
+export const info = (message, config) => {
+  toast.info(message, config);
+};
+export const warning = ({ title, text }, config) => {
+  toast.warning(h('div', {}, [h('h2', {}, title || ''), h('p', {}, text || '')]), config);
+};
+
+export default { error, success, info, warning };
